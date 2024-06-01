604892a40f61ae0a10fc6e0fd845a8a0cb875c7e
Gian Gonzaga <gian@giangonzaga.com>
2024-06-02 04:28:58 +0800

    feat: add insertion to supabase tables

--- supabase.ts
+++ supabase.ts
@@ -31,13 +31,99 @@ const uploadImages = async (vehicleMetadata: VehicleMetadataWithVariants) => {
   });
 };
 
+const uploadVariants = async (vehicleMetadata: VehicleMetadataWithVariants) => {
+  const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  const formattedName = vehicleMetadata.name?.replace(/\s/g, "-").toLowerCase();
+
+  try {
+    const { data: newVariant, error } = await supabase
+      .from("vehicle_variants")
+      .upsert({
+        name: vehicleMetadata.name,
+        url: vehicleMetadata.url,
+        metadata_url: vehicleMetadata.metadataUrl,
+      })
+      .select()
+      .maybeSingle();
+
+    if (error) {
+      throw error;
+    }
+
+    console.log("Uploaded vehicle:", vehicleMetadata.name);
+
+    for (const variant of vehicleMetadata.variants!) {
+      const { data: newVariantMetadata, error } = await supabase
+        .from("vehicle_variant_metadata")
+        .upsert({
+          vehicle_id: newVariant.id,
+          variant_id: variant.id,
+          code: variant.code,
+          name: variant.name,
+          transmission: variant.transmission,
+          fuel_type: variant.fuelType,
+          seating_capacity: variant.seatingCapacity,
+          model: variant.model,
+          type: variant.type,
+          displacement: variant.displacement,
+          fuel_capacity: variant.fuelCapacity,
+          power_transmission: variant.powerTransmission,
+          tires: variant.tires,
+          wheels: variant.wheels,
+          front_rear_brake: variant.frontRearBrake,
+        })
+        .select()
+        .maybeSingle();
+
+      const variantImages = vehicleMetadata.images!.filter(
+        (image) => image.variantId === variant.id
+      );
+
+      if (variantImages) {
+        for (const image of variantImages) {
+          const filename = `${formattedName}_${
+            image.variantId
+          }_${image.alt?.replace(/\s/g, "")}.png`.replace(
+            /[<>:"/\\|?*\x00-\x1F]/g,
+            ""
+          );
+          const path = `cars/${filename}`;
+
+          const { error } = await supabase
+            .from("vehicle_variant_images")
+            .upsert({
+              vehicle_variant_id: newVariant.id,
+              variant_metadata_id: newVariantMetadata.id,
+              url: path,
+              alt: image.alt,
+            });
+
+          if (error) {
+            console.error("Error uploading image:", error);
+          } else {
+            console.log("Uploaded image:", path);
+          }
+        }
+      }
+
+      console.log("Uploaded variant:", variant.name);
+      if (error) {
+        throw error;
+      }
+    }
+  } catch (error) {
+    console.error("Error uploading vehicle:", error);
+  }
+};
+
 async function main() {
   const vehicleMetadataWithVariants = JSON.parse(
     fs.readFileSync("resources/vehicles.json", "utf-8")
   ) as VehicleMetadataWithVariants[];
 
   for (const vehicleMetadata of vehicleMetadataWithVariants) {
-    await uploadImages(vehicleMetadata);
+    // await uploadImages(vehicleMetadata);
+    await uploadVariants(vehicleMetadata);
   }
 }
 
